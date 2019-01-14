dfb2dd42603b279857311d1261f59e86beed5db5
Wang Zhenguo <pricss@126.com m>
2019-01-14 17:38:42 +0800

    update the colour selection

--- Lesson5_4 Colour selection code example/ColourSelection.py
+++ Lesson5_4 Colour selection code example/ColourSelection.py
@@ -11,26 +11,21 @@ xsize = image.shape[1]
 
 color_select = np.copy(image)
 
-red_threshold = 0
-green_threshold = 0
-blue_threshold = 0
+red_threshold = 200
+green_threshold = 200
+blue_threshold = 200
 rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 
 thresholds = (image[:, :, 0] < rgb_threshold[0]) | (image[:, :, 1] < rgb_threshold[1]) | (image[:, :, 2] < rgb_threshold[2])
 
-
 color_select[thresholds] = [0, 0, 0]
 
 # Display the image
-#plt.imshow(color_select)
-
-plt.figure(1)
-plt.imshow(image[:,:,2])
-
-plt.figure(2)
-plt.imshow(image[:, :,0])
-
-
+plt.figure('Colour Selection')
+plt.subplot(221), plt.title('Original')
+plt.imshow(image)
+plt.subplot(222), plt.title('After Colour Selection')
+plt.imshow(color_select)
 
 plt.show()
 
